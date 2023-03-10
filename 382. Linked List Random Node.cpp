27ffc72d46ad17a980f84af20fcf5c590bdd30d5
mohamedaichaouiii <111172566+mohamedaichaouiii@users.noreply.github.com>
2023-03-10 07:10:24 +0100

    Day 10

---
+++ 382. Linked List Random Node.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+    ListNode* head;
+public:
+    Solution(ListNode* head) {
+        this->head = head;
+    }
+    
+    int getRandom() {
+        ListNode* curr = head;
+        int i = 1;
+        int res = 0;
+        while (curr != nullptr) {
+            if (rand() % i == 0) {
+                // take this value
+                res = curr->val;
+            }
+            i++;
+            curr = curr->next;
+        }
+        return res;
+    }
+};
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
